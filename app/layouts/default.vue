3a005a55201906ef54a00b8e8d8c918fdcf6172c
Benjamin Canac <canacb1@gmail.com>
2026-08-12 18:08:48 +0200

    fix(app): update logo

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -6,7 +6,7 @@
       class="p-2"
       :ui="{
         inner: 'bg-elevated/25',
-        header: 'px-4',
+        header: 'px-2',
         body: 'p-2 gap-4',
         footer: 'p-2'
       }"
@@ -17,7 +17,8 @@
           aria-label="Home"
           class="flex items-end gap-0.5 text-highlighted"
         >
-          <AppLogo class="w-auto h-6 shrink-0" />
+          <AppLogo class="h-8 w-auto shrink-0" />
+          <span class="text-xl font-bold text-highlighted">Calendar</span>
         </NuxtLink>
       </template>
 
