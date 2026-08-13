d98260fbd2048777fea54ae92f7c4da45dbec753
Benjamin Canac <canacb1@gmail.com>
2026-08-13 15:41:43 +0200

    style(app): frost the sidebar

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,16 +3,15 @@ const { createEvent, isCommandPaletteOpen } = useCalendar()
 </script>
 
 <template>
-  <div class="flex h-svh overflow-hidden">
+  <div class="isolate relative flex h-svh overflow-hidden">
     <USidebar
       variant="floating"
       collapsible="none"
       class="p-2"
       :ui="{
-        inner: 'bg-elevated/25',
-        header: 'px-2',
-        body: 'p-2 gap-4',
-        footer: 'p-2'
+        inner: 'bg-white/50 dark:bg-neutral-950/25 backdrop-blur-xl backdrop-saturate-200 rounded-xl divide-none',
+        body: 'p-2 pt-0 gap-2',
+        footer: 'p-2 border-t border-default'
       }"
     >
       <template #header>
@@ -25,7 +24,7 @@ const { createEvent, isCommandPaletteOpen } = useCalendar()
           <span class="text-xl font-bold text-highlighted">Calendar</span>
         </NuxtLink>
 
-        <div class="ms-auto flex items-center gap-1">
+        <div class="ms-auto flex items-center gap-1.5">
           <UTooltip
             text="Search"
             :kbds="['meta', 'k']"
@@ -33,7 +32,7 @@ const { createEvent, isCommandPaletteOpen } = useCalendar()
             <UButton
               icon="i-lucide-search"
               color="neutral"
-              variant="soft"
+              variant="outline"
               aria-label="Search"
               size="sm"
               class="rounded-full"
@@ -55,6 +54,8 @@ const { createEvent, isCommandPaletteOpen } = useCalendar()
           </UTooltip>
         </div>
       </template>
+
+      <div class="pointer-events-none absolute -inset-e-32 -top-32 -z-10 size-124 rounded-full bg-primary/2.5 blur-3xl" />
 
       <CalendarList />
 
